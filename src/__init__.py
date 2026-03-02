f0c54c7ed89de0bd96f1d9b5888138659fc34f43
Preetish Ubhrani <ubhranipreetish@gmail.com>
2026-03-02 16:38:37 +0530

    Added Confusion Matrix Images

---
+++ src/__init__.py
@@ -0,0 +1,18 @@
+NUMERICAL_COLS = [
+    'person_age',
+    'person_income',
+    'person_emp_length',
+    'loan_amnt',
+    'loan_int_rate',
+    'loan_percent_income',
+    'cb_person_cred_hist_length'
+]
+
+CATEGORICAL_COLS = [
+    'person_home_ownership',
+    'loan_intent',
+    'loan_grade',
+    'cb_person_default_on_file'
+]
+
+TARGET_COL = 'loan_status'
